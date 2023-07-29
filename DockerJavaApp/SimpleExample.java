7352fda2bf4d5578ba45c2110be28052bbfd2449
Phaniraj <79626160+phani-training@users.noreply.github.com>
2023-07-29 17:48:45 +0530

    Add files via upload

---
+++ DockerJavaApp/SimpleExample.java
@@ -0,0 +1,9 @@
+
+public class SimpleExample {
+    public static void main(String[] args) {
+         System.out.println("hello world from java");   
+    }
+}
+
+//docker build -t java-app .
+//docker run --name myApp java-app 
